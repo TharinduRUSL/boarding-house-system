b6edee61935931e179f75b2cfc94392cd816d428
Tharindu Sampath <sampaththarindu119@gmail.com>
2026-05-19 03:48:45 -0700

    Add first backend test API

---
+++ backend/src/main/java/com/boardinghouse/backend/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.boardinghouse.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String testApi() {
+        return "Boarding House Backend is running successfully!";
+    }
+}
